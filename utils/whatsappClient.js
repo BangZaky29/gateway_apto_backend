c7ab0f7ba62214677d44e2eddae8e8617cd65742
BangZaky0029 <bangzaky0029@gmail.com>
2026-01-07 11:22:49 +0700

    update

--- utils/whatsappClient.js
+++ utils/whatsappClient.js
@@ -27,16 +27,18 @@ class WhatsAppClient {
       }),
       puppeteer: {
         headless: true,
+        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
         args: [
-          '--no-sandbox',
-          '--disable-setuid-sandbox',
-          '--disable-dev-shm-usage',
-          '--disable-accelerated-2d-canvas',
-          '--no-first-run',
-          '--no-zygote',
-          '--disable-gpu'
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--disable-gpu'
         ]
-      }
+        }
+
     });
 
     // QR Code generated
